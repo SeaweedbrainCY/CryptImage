9c0df731a2dea5b1089d128b863b44b5521945f6
Nathan Stchepinsky <nathanstchepinsky@protonmail.com>
2022-03-23 02:13:39 +0100

    Re construction des fonctions d'embed et d'extraction de LSB Correction des très nombreux bug et restructure totale de l'extraction AJout de la gestion de fin d'image pour l'extraction mais toujours pas pour l'embed

--- cryptimage/lsb.py
+++ cryptimage/lsb.py
@@ -7,6 +7,8 @@ from cryptimage.watermark import Watermark
 from PIL import Image
 import base64
 import json
+import sys
+from hashlib import *
 class LSB(Watermark):
     lsb_str = "" # The string to embed in LSB
 
@@ -32,7 +34,7 @@ class LSB(Watermark):
         encrypted = aes.encrypt(position_str).decode()
         
         signed = crypto.sign(encrypted)
-        self.lsb_str = encrypted + "&" + signed + "&"
+        self.lsb_str = encrypted + "&" + signed.strip() + "&"
         
 
     """
@@ -61,13 +63,12 @@ class LSB(Watermark):
 
         
 
-
     """
        Code le message dans la premiere colonne de pixel de l'image
     """
     def embedInLSB(self):
         #Importation de l'image a encoder
-        im = Image.open("/Users/stchepinskynathan/Downloads/test.png")
+        im = Image.open(self.imageURL)
         width, height = im.size
         pixels = im.load()
 
@@ -75,75 +76,110 @@ class LSB(Watermark):
             raise Exception("FATAL ERROR : There is no string to embed in LSB") 
 
         #Conversion en binaire du Str à encoder
-        bin_lsb_str = ''.join(format(ord(x), 'b').zfill(8) for x in self.lsb_str)        
+        bin_lsb_str = ''.join(format(ord(x), 'b').zfill(8) for x in self.lsb_str)   
+        print(' '.join(format(ord(x), 'b').zfill(8) for x in self.lsb_str))     
         i = 0
-        current_line = 0
+        x=0
+        y = 0
         #On parcourt la premiere ligne de pixel de longueur notre message
         print(len(bin_lsb_str))
-        for x in range(0, len(bin_lsb_str), 3):  
+        print(self.lsb_str)
+        for i in range(0, len(bin_lsb_str), 3):  
             #print(x)            
             if x> width: # on a atteint le bout de la ligne, on passe à la ligne suivante 
-                current_line += 1
-
-            x = x - int(current_line * width)
-            r,g,b,_=pixels[x,current_line]  
-            if i<len(bin_lsb_str):
-                r_bit, g_bit, b_bit = bin(r), bin(g), bin(b)
+                y += 1
+                x=0
+                print("SAUT DE LIGNE")
+            if im.mode == "RGB":
+                r,g,b=pixels[x,y]
+            else :
+                r,g,b,a =pixels[x,y]
+            #print(x,y, " : ", r,g,b)  
+            
+            r_bit, g_bit, b_bit = bin(r), bin(g), bin(b)
 
                 #On extrait les LSB de chaque code rgb
                 #r_lsb, g_lsb, b_lsb = int(r_bit[-1]), int(g_bit[-1]), int(b_bit[-1])           
 
                 #On modifie chaque R G B avec le code voulu
-                new_r_lsb, new_g_lsb, new_b_lsb = bin_lsb_str[i], bin_lsb_str[i+1], bin_lsb_str[i+2]       
+            final_embed_b_bit = b
+            final_embed_g_bit = g 
+            final_embed_r_bit = r  
                 
                 #On reconstitue nos octets pour chaque R G B
-                final_embed_r_bit = int(r_bit[:-1] + str(new_r_lsb), 2)
-                final_embed_g_bit = int(g_bit[:-1] + str(new_g_lsb), 2)
-                final_embed_b_bit = int(b_bit[:-1] + str(new_b_lsb), 2)
+            if i < len(bin_lsb_str):
+                final_embed_r_bit = int(r_bit[:-1] + str(bin_lsb_str[i]), 2)
+            if i+1 < len(bin_lsb_str):
+                final_embed_g_bit = int(g_bit[:-1] + str(bin_lsb_str[i+1]), 2)
+            if i+2 < len(bin_lsb_str):
+                final_embed_b_bit = int(b_bit[:-1] + str(bin_lsb_str[i+2]), 2)
+
+            #print("initial = ", r_bit, g_bit, b_bit, end=' ')
+            if i+2 < len(bin_lsb_str):
+                print(bin( final_embed_r_bit ),bin(final_embed_g_bit) , bin(final_embed_b_bit) , end=' ')
 
 
-        #On code nos octets dans l'image
-        pixels[x,0] = (final_embed_r_bit, final_embed_g_bit, final_embed_b_bit)
-        print(pixels[x,0])
-        
-        im.save("original.png")
+            #On code nos octets dans l'image
+            #print(bin_lsb_str[i],bin_lsb_str[i+1] ,bin_lsb_str[i+2], end='')
+            if im.mode == "RGB" :
+                pixels[x,y] = (final_embed_r_bit, final_embed_g_bit, final_embed_b_bit) 
+            else :
+                pixels[x,y] = (final_embed_r_bit, final_embed_g_bit, final_embed_b_bit,a) 
+                
+            x+=1
+            #print(pixels[x,y], end=' ') 
+        print(self.lsb_str)
+       
+        im.save(self.finalImageURL)
+        #print(self.md5f("original.png"))
         #Penser a passer a la ligne si on depasse la longueur de l'image par rapport a la longueur du 
 
     """
         Decode le message contenu dans les LSB de la premiere colonne de pixel de l'image
     """
     def decodeLSB(self):
-        im = Image.open("original.png") #TEST1 iwQnPxC+du2YxYmkdmDOFuIUgt2fOu/Mwn+YxaViYZo=
-
+        im = Image.open(self.imageURL) #TEST1 iwQnPxC+du2YxYmkdmDOFuIUgt2fOu/Mwn+YxaViYZo=
+        width, height = im.size
         chars = ""
         extracted = ""
         pixels = im.load()
-
-        c = 0       #Compteur du caractere d'arret
-
-        for x in range(0,im.width): 
-            r,g,b,_= pixels[x,0]
-
-            #On extrait les LSB de chaque pixel (R G B)
-            extracted += bin(r)[-1]
-            extracted += bin(g)[-1]
-            extracted += bin(b)[-1]
-
-            if(len(extracted)==8):
-
-                #Décodage binaire
-                chars.append(chr(int(''.join([str(bit) for bit in extracted]), 2)))
-
-                #Verification de caractère d'arret
-                if chars[-1] == "&":
-                    if c == 0:
-                        c +=1
-                    else:
-                        pass
         
-        
-        self.lsb_str = chars
-        print(self.lsb_str)
+        spliter_counter = 0
+        x,y = 0,0
+        while spliter_counter != 2: 
+            if x >= width:
+                y+= 1
+                x=0
+            if y >= height :
+                raise Exception("FATAL ERROR : Impossible to retrieve hidden data. The file must be corrupted or not protected")
+            if im.mode == "RGB":
+                r,g,b = pixels[x,y] 
+            else :
+                r,g,b,_ = pixels[x,y] 
+            #print(pixels[x,y], end=' ')
+            r_bit = str(bin(r))[2:][-1]
+            g_bit = str(bin(g))[2:][-1]
+            b_bit = str(bin(b))[2:][-1]
+            print(bin(r),bin(g), bin(b), end=' ')
+
+            lsbits = [r_bit, g_bit, b_bit]
+
+            for bit in lsbits:
+                chars += bit
+
+                if len(chars) == 8 : # On a un octet alors on convertit
+                    #print(chars)
+                    new_char = "".join([chr(int(chars[i:i+8], 2)) for i in range(0,len(chars),8)]) 
+                    extracted += new_char
+                    chars = ""
+                    if new_char == "&":
+                        spliter_counter += 1
+                    #print(extracted)
+            x+=1
+        print(extracted)
+        self.lsb_str = extracted
+
+                
 
 
 
